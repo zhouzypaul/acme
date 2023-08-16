e178347b42b7a594b07e00113f980d63e1a9b12a
Akhil Bagaria <abagaria@hmc.edu>
2023-08-16 17:38:26 -0400

    updated plotting script.

--- plotting_script.py
+++ plotting_script.py
@@ -1,23 +1,51 @@
 import argparse
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 
+experiment_names = [
+  # 'four_rooms_exploration_prob0_uniform_node_selection',
+  # 'four_rooms_exploration_prob0_1_novelty_node_selection',
+  'four_rooms_exploration_after_exploitation2',
+  # 'four_rooms_developing_sparse_nodes_task_goal_prob0pt1_long_traj_her_random_exploration',
+  'four_rooms_big_refactor',
+  'four_rooms_big_refactor_no_exploration_rollouts'
+]
+
+exp2label = {
+  'four_rooms_exploration_after_exploitation2': 'Shared UVFA for Exploration',
+  'four_rooms_big_refactor': 'Random Exploration',
+  'four_rooms_big_refactor_no_exploration_rollouts': 'No Separate Exploration Rollout'
+}
+
+
 parser = argparse.ArgumentParser()
-parser.add_argument('--experiment_name', type=str, default='32actors-cpu-spi-1-toy')
+parser.add_argument('--save_filename', type=str, required=True)
 args = parser.parse_args()
 
 
+def moving_average(a, n=25):
+  ret = np.cumsum(a, dtype=float)
+  ret[n:] = ret[n:] - ret[:-n]
+  return ret[n-1:] / n
+
+
 def plot_evaluation_return():
-  filename = f'/home/akhil/acme/{args.experiment_name}/logs/evaluator/logs.csv'
-  df = pd.read_csv(filename)
-  plt.plot(df['evaluator_steps'], df['episode_return'], label='Evaluation return')
+  for experiment_name in experiment_names:
+    filename = f'/home/akhil/acme/{experiment_name}/logs/evaluator/logs.csv'
+    df = pd.read_csv(filename)
+    # plt.plot(df['evaluator_steps'], df['episode_return'], label=exp2label[experiment_name])
+    plt.plot(moving_average(df['episode_return'].to_numpy(), n=100), label=exp2label[experiment_name])
+  
+  plt.legend()
   plt.xlabel('Evaluator Steps')
   plt.ylabel('Episode Return')
-  plt.savefig(f'{args.experiment_name}_eval_return.png')
+  plt.title('Evaluator Return')
+  plt.xlim((0, 2000))
+  print(f'Saving {args.save_filename}.png')
+  plt.savefig(f'{args.save_filename}.png')
   plt.close()
-  
 
-def visualize_value_function(path_to_value_matrix: str):
-  pass
 
+plot_evaluation_return()
