e2b6d89f7b66d93f8563fd67b4c009f2aeb82b36
Akhil Bagaria <abagaria@hmc.edu>
2023-09-20 21:24:56 -0400

    [GSM] incrementally keep track of value/transition matrix and solve it for each actor. Was able to get the begin_episode() time way down to a couple of seconds when the goal space goes to about 2500 nodes.

--- acme/agents/jax/r2d2/gsm.py
+++ acme/agents/jax/r2d2/gsm.py
@@ -1,7 +1,4 @@
 import time
-import ipdb
-import copy
-import pickle
 import pprint
 import random
 import dm_env
@@ -18,10 +15,11 @@ from acme.wrappers.oar_goal import OARG
 from acme.agents.jax.r2d2 import networks as r2d2_networks
 from acme.jax import variable_utils
 from acme.jax import networks as networks_lib
-from acme.core import VariableSource, Saveable
+from acme.core import Saveable
+from acme.agents.jax.r2d2.goal_sampler import GoalSampler
 
 
-class GoalSpaceManager(VariableSource, Saveable):
+class GoalSpaceManager(Saveable):
   """Worker that maintains the skill-graph."""
 
   def __init__(
@@ -30,6 +28,7 @@ class GoalSpaceManager(VariableSource, Saveable):
       rng_key: networks_lib.PRNGKey,
       networks: r2d2_networks.R2D2Networks,
       variable_client: Optional[variable_utils.VariableClient],
+      tensor_increments: int = 1000,
     ):
     self._environment = environment
 
@@ -46,23 +45,66 @@ class GoalSpaceManager(VariableSource, Saveable):
     self._hash2reward = {}
     self._hash2discount = {}
     
-    # src goal hash -> dest goal hash -> value
-    self._value_matrix = collections.defaultdict(
-      lambda : collections.defaultdict(lambda: 1.))
+    self._tensor_increments = tensor_increments
+    self._n_actions = tensor_increments
+    self._n_states = self._n_actions
+    
+    self._transition_matrix = np.zeros(
+      (self._n_states, self._n_actions), dtype=np.float32)
+    
+    self._hash2idx = {}
+    self._idx2hash = {}
+    self._idx_dict_lock = threading.Lock()
     
     self._rng_key = rng_key
     self._variable_client = variable_client
     self._networks = networks
-    self._n_parameter_updates = 0
     self._iteration_iterator = itertools.count()
+    self._gsm_loop_last_timestamp = time.time()
 
-  def get_variables(self, names):
+  def begin_episode(self, current_node: Tuple) -> Tuple[Tuple, Dict]:
+    """Create and solve the AMDP. Then return the abstract policy."""
+    goal_sampler = GoalSampler(
+      *self.get_variables(),
+      task_goal_probability=0.1,
+      task_goal=self.task_goal,
+      exploration_goal=self.exploration_goal,
+      exploration_goal_probability=0.
+    )
+    expansion_node = goal_sampler.begin_episode(current_node)
+    abstract_policy = goal_sampler._amdp.get_policy()
+
+    return expansion_node, abstract_policy
+  
+  def get_descendants(self, current_node: Tuple): 
+    return GoalSampler(
+      *self.get_variables(),
+      task_goal_probability=0.1,
+      task_goal=self.task_goal,
+      exploration_goal=self.exploration_goal,
+      exploration_goal_probability=0.
+    ).get_descendants(current_node)
+
+  def get_variables(self, names=()):
     del names
-    return self.get_value_dict(),\
-          self.get_goal_dict(),\
+    hash2idx = self._thread_safe_deepcopy(self._hash2idx)
+    n_actions = self._transition_matrix.shape[1]
+    n_nodes = min(n_actions, len(hash2idx))
+    hash2idx = {k: v for k, v in hash2idx.items() if v < n_actions}
+    transition_tensor = self.get_transition_tensor(n_nodes)
+    return self.get_goal_dict(),\
           self.get_count_dict(),\
           self.get_on_policy_count_dict(),\
-          *self.get_extrinsic_reward_dicts()
+          *self.get_extrinsic_reward_dicts(),\
+          hash2idx,\
+          transition_tensor,\
+          self._thread_safe_deepcopy(self._idx2hash)
+    
+  # TODO(ab): lock transition matrix during the copy operation
+  def get_transition_tensor(self, n_actions: int):
+    n_states = n_actions
+    actual_transition_matrix = self._transition_matrix[:n_states, :n_actions].copy()
+    return actual_transition_matrix
 
   @property
   def task_goal(self) -> OARG:
@@ -96,9 +138,12 @@ class GoalSpaceManager(VariableSource, Saveable):
   def update_params(self, wait: bool = False):
     """Update to a more recent copy of the learner params."""
     if self._variable_client:
+      t0 = time.time()
       self._variable_client.update(wait)
-      self._n_parameter_updates += 1
-      
+      print(f'[GSM] Took {time.time() - t0}s to update GSM learner params.',
+            f'VarUpdatePeriod={self._variable_client._update_period.total_seconds()}s',
+            f'TimeSinceLastVarUpdate={time.time() - self._variable_client._last_call}s')
+
   def goal_reward_func(self, current: OARG, goal: OARG) -> Tuple[bool, float]:
     """Is the goal achieved in the current state."""
     dims = np.where(goal.goals >= 0)
@@ -132,24 +177,25 @@ class GoalSpaceManager(VariableSource, Saveable):
          goal[:, :, :n_goal_dims]), axis=-1)
     raise NotImplementedError(method)
   
+  def is_special_context(self, hash: Tuple) -> bool:
+    return hash == tuple(self.exploration_goal.goals) or hash == tuple(self.task_goal.goals)
+  
   def get_recurrent_state(self, batch_size=None):
     return self._networks.init_recurrent_state(self._rng_key, batch_size)
   
-  def get_count_dict(self):
-    return self._hash2counts
+  def get_count_dict(self) -> Dict:
+    return self._thread_safe_deepcopy(self._hash2counts)
   
   def get_on_policy_count_dict(self):
-    return self._default_dict_to_dict(self._on_policy_counts)
+    with self._on_policy_count_dict_lock:
+      return self._default_dict_to_dict(self._on_policy_counts)
   
   def get_goal_dict(self):
-    return self._hash2obs
-  
-  def get_value_dict(self) -> dict:
-    """Convert to regular dict because courier cannot handle fancy dicts."""
-    return self._default_dict_to_dict(self._value_matrix)
+    return self._thread_safe_deepcopy(self._hash2obs)
   
   def get_extrinsic_reward_dicts(self) -> Tuple[Dict, Dict]:
-    return self._hash2reward, self._hash2discount
+    return self._thread_safe_deepcopy(self._hash2reward),\
+      self._thread_safe_deepcopy(self._hash2discount)
     
   def update(
     self,
@@ -162,6 +208,7 @@ class GoalSpaceManager(VariableSource, Saveable):
     self._update_obs_dict(hash2obs)
     self._update_count_dict(hash2count)
     self._update_on_policy_count_dict(edge2count)
+    self._update_idx_dict(hash2obs)
     self._hash2discount.update(hash2discount)
     
   def _update_count_dict(self, hash2count: Dict):
@@ -180,6 +227,14 @@ class GoalSpaceManager(VariableSource, Saveable):
       for key in hash2count:
         src, dest = key
         self._on_policy_counts[src][dest] += hash2count[key]
+
+  def _update_idx_dict(self, hash2obs: Dict):
+    with self._idx_dict_lock:
+      for hash in hash2obs:
+        if hash not in self._hash2idx:
+          idx = len(self._hash2idx)
+          self._hash2idx[hash] = idx
+          self._idx2hash[idx] = hash
   
   def _construct_oarg(self, obs, action, reward, goal_features) -> OARG:
     """Convert the obs, action, etc from the GSM into an OARG object.
@@ -196,16 +251,41 @@ class GoalSpaceManager(VariableSource, Saveable):
       reward=reward,
       goals=np.asarray(goal_features, dtype=np.int16)
     )
-        
-  def _update_value_dict(self, src_dest_pairs, values):
-    """Make a probabilistic graph in goal-space."""
+
+  # TODO(ab): lock the transition matrix while updating it.
+  def _update_transition_tensor(self, src_dest_pairs, values):
     for (src, dest), value in zip(src_dest_pairs, values):
-      self._value_matrix[src][dest] = value.item()
+      if not self.is_special_context(dest) and \
+        src in self._hash2idx and dest in self._hash2idx:
+        src_idx = self._hash2idx[src]
+        dest_idx = self._hash2idx[dest]
+        
+        # TODO(ab): incorporate count-based bonus.
+        prob = np.clip(value, 0., 1.)
+        self._transition_matrix[src_idx, dest_idx] = prob
+    
+    if len(self._hash2idx) >= self._n_actions:
+      self._resize_transition_tensor()
+
+  def _resize_transition_tensor(self):
+    """Dynamically resize the transition tensor."""
+    t0 = time.time()
+    n_actions = self._n_actions * 2
+    n_states = n_actions
+    old_transition_matrix = self._transition_matrix.copy()
+    self._transition_matrix = np.zeros(
+      (n_states, n_actions),
+      dtype=self._transition_matrix.dtype)
+    self._transition_matrix[
+      :old_transition_matrix.shape[0],
+      :old_transition_matrix.shape[1]
+    ] = old_transition_matrix
+    self._n_states, self._n_actions = self._transition_matrix.shape
+    print(f'[GSM] Resized transition tensor to {self._transition_matrix.shape} in {time.time() - t0}s')
         
   def _construct_obs_matrix(self):
-    def get_nodes():
-      hash2oar = self.get_goal_dict()
-      return {k: v for k, v in list(hash2oar.items())}
+    def get_nodes():  # TODO(ab): is this just a deepcopy of hash2goal?
+      return self.get_goal_dict()
 
     def get_exploration_node():
       exploration_oarg = self.exploration_goal
@@ -215,6 +295,7 @@ class GoalSpaceManager(VariableSource, Saveable):
       task_goal = self.task_goal
       return {tuple(task_goal.goals): task_goal}
     
+    # TODO(ab): why get all nodes if we are going to subsample 50?
     t0 = time.time()
     nodes = get_nodes()
     t1 = time.time()
@@ -240,7 +321,7 @@ class GoalSpaceManager(VariableSource, Saveable):
     src_dest_pairs = []
         
     for src in nodes:
-      for dest in dest_nodes:  # TODO(ab): Include explore/exploit VFs here.
+      for dest in dest_nodes:
         obs = nodes[src]
         goal = dest_nodes[dest]
         oarg = self.obs_augment_fn(obs, goal, 'concat')[0]
@@ -275,8 +356,9 @@ class GoalSpaceManager(VariableSource, Saveable):
   @staticmethod
   def _default_dict_to_dict(dd):
     d = {}
-    for key, val in dd.items():
-      d[key] = dict(val)
+    keys = list(dd.keys())
+    for key in keys:
+      d[key] = dict(dd[key])
     return d
   
   @staticmethod
@@ -287,25 +369,34 @@ class GoalSpaceManager(VariableSource, Saveable):
       dd[key] = collections.defaultdict(
         inner_default, nested_dict[key])
     return dd
-    
-  def _save_value_dict(self, iteration):   
-    t0 = time.time()
-    print('Saving the value matrix..')
-    with open(f'value_matrix_iteration_{iteration}.pkl', 'wb+') as f:
-      pickle.dump(self._default_dict_to_dict(self._value_matrix), f)
-    print(f'Took {time.time() - t0}s to save value matrix.')
+  
+  @staticmethod
+  def _thread_safe_deepcopy(d: Dict):
+    # Technically size of d can change during list(d.keys()),
+    # but low prob and hasn't happened yet.
+    keys = list(d.keys())
+    return {k: d[k] for k in keys}
 
   def visualize_value_function(
     self,
-    value_dict: Dict,
     episode: int,
-    n_goal_subplots: int = 4, 
-    plot_task_value_fn: bool = True,
-    plot_explore_value_fn: bool = True
+    task_value_vector: Optional[np.ndarray] = None,
+    exploration_value_vector: Optional[np.ndarray] = None,
+    n_goal_subplots: int = 4
   ):
     assert n_goal_subplots % 2 == 0, 'Ask for even number of subplots.'
+    assert task_value_vector is None or len(task_value_vector.shape) == 1
+    assert exploration_value_vector is None or len(exploration_value_vector.shape) == 1
 
-    starts = list(value_dict.keys())
+    plot_task_value_fn = task_value_vector is not None
+    plot_explore_value_fn = exploration_value_vector is not None
+
+    def _get_value(src: Tuple, dest: Tuple):
+      row = self._hash2idx[src]
+      col = self._hash2idx[dest]
+      return self._transition_matrix[row, col]
+
+    starts = list(self._hash2idx.keys())
     
     if len(starts) < n_goal_subplots:
       return
@@ -318,41 +409,37 @@ class GoalSpaceManager(VariableSource, Saveable):
 
     for i, start in enumerate(selected_starts):
       xs = []; ys = []; values = []
-      for (x, y, key, door) in value_dict[start]:
-        xs.append(x)
-        ys.append(y)
-        value = min(value_dict[start][(x, y, key, door)], 1)
+      for goal_hash in starts:
+        xs.append(goal_hash[0])
+        ys.append(goal_hash[1])
+        value = _get_value(start, goal_hash)
         values.append(value)
-      plt.subplot(n_subplots // 2, n_subplots // 3, i + 1)
+      plt.subplot(n_subplots // 2, n_subplots // 2, i + 1)
       plt.scatter(xs, ys, c=values)
       plt.colorbar()
       plt.title(f'Start State: {start}')
 
     if plot_task_value_fn:
       xs = []; ys = []; values = []
-      task_goal_feats = tuple(self.task_goal.goals)
       for start in starts:
-        (x, y, key, door) = start
-        xs.append(x)
-        ys.append(y)
-        values.append(value_dict[start][task_goal_feats])
-      plt.subplot(n_subplots // 2, n_subplots // 3, n_goal_subplots + 1)
+        xs.append(start[0])
+        ys.append(start[1])
+        values.append(task_value_vector[self._hash2idx[start]])
+      plt.subplot(n_subplots // 2, n_subplots // 2, n_goal_subplots + 1)
       plt.scatter(xs, ys, c=values)
       plt.colorbar()
-      plt.title(f'Task Goal ({task_goal_feats})')
+      plt.title('Task Reward Function')
 
     if plot_explore_value_fn:
       xs = []; ys = []; values = []
-      explore_goal_feats = tuple(self.exploration_goal.goals)
       for start in starts:
-        (x, y, key, door) = start
-        xs.append(x)
-        ys.append(y)
-        values.append(value_dict[start][explore_goal_feats])
-      plt.subplot(n_subplots // 2, n_subplots // 3, n_goal_subplots + 2)
+        xs.append(start[0])
+        ys.append(start[1])
+        values.append(exploration_value_vector[self._hash2idx[start]])
+      plt.subplot(n_subplots // 2, n_subplots // 2, n_goal_subplots + 2)
       plt.scatter(xs, ys, c=values)
       plt.colorbar()
-      plt.title(f'Exploration Goal ({explore_goal_feats})')
+      plt.title('Exploration Context')
 
     plt.savefig(f'plots/uvfa_plots/four_rooms_uvfa_episode_{episode}.png')
     plt.close()
@@ -377,19 +464,31 @@ class GoalSpaceManager(VariableSource, Saveable):
       values = values.max(axis=-1)[0]  # (1, B, |A|) -> (1, B) -> (B,)
       
       print(f'[iteration={iteration}] values={values.shape}, max={values.max()} dt={time.time() - t0}s')
-      self._update_value_dict(src_dest_pairs, values)
+      self._update_transition_tensor(src_dest_pairs, values)
 
-      if iteration > 0 and iteration % 100 == 0:
-        # self._save_value_dict(iteration)
-        self.visualize_value_function(self._value_matrix, iteration)
+      if iteration > 0 and iteration % 10 == 0:
+        self.visualize_value_function(
+          iteration,
+          task_value_vector=None,
+          exploration_value_vector=None
+        )
 
       if len(src_dest_pairs) > 1000:
-        # start_state_features = (1, 5, 0, 0)  # TODO(ab): get from env and pass around
-        start_state_features = (2, 10, 0, 0)
-        # start_state_features = (8, 16, 0, 0)
-        pprint.pprint(self._value_matrix[start_state_features])
+        # TODO(ab): get from env and pass around
+        # start_state_features = (1, 5, 0, 0)  
+        start_state_features = (8, 16, 0, 0, 0, 0, 0, 0, 0, 0, 0)  # FourRooms
+        # start_state_features = (2, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0)  # DoorKey
+        # start_state_features = (3, 1, 0, 1, 2, 0, 0, 0, 0, 0, 0)  # S3R1
+        # start_state_features = (7, 7, 0, 1, 1, 1, 1, 2, 1, 0, 0)  # S5R3
+        if start_state_features in self._hash2idx:
+          row_idx = self._hash2idx[start_state_features]
+          pprint.pprint(self._transition_matrix[row_idx, :len(self._hash2idx)])
 
-      print(f'Iteration {iteration} Goal Space Size {len(self._hash2obs)}')
+      dt = time.time() - self._gsm_loop_last_timestamp
+      print(f'Iteration {iteration} Goal Space Size {len(self._hash2obs)} dt={dt}')
+      self._gsm_loop_last_timestamp = time.time()
+
+      self.update_params(wait=False)
 
   def run(self):
     for iteration in self._iteration_iterator:
@@ -399,21 +498,32 @@ class GoalSpaceManager(VariableSource, Saveable):
   def save(self) -> Tuple[Dict]:
     t0 = time.time()
     print('[GSM] Checkpointing..')
-    to_return = self.get_variables(names=[])
-    assert len(to_return) == 6, len(to_return)
+    to_return = self.get_variables()
+    assert len(to_return) == 8, len(to_return)
     print(f'[GSM] Checkpointing took {time.time() - t0}s.')
     return to_return
 
   def restore(self, state: Tuple[Dict]):
     t0 = time.time()
     print('About to start restoring GSM from checkpoint.')
-    assert len(state) == 6, len(state)
-    ipdb.set_trace()
-    self._value_matrix = collections.defaultdict(
-      lambda : collections.defaultdict(lambda: 1.), state[0])
-    self._hash2obs = state[1]
-    self._hash2counts = state[2]
-    self._on_policy_counts = self._dict_to_default_dict(state[3], int)
-    self._hash2reward = state[4]
-    self._hash2discount = state[5]
+    assert len(state) == 8, len(state)
+    self._hash2obs = state[0]
+    self._hash2counts = collections.defaultdict(int, state[1])
+    self._on_policy_counts = self._dict_to_default_dict(state[2], int)
+    self._hash2reward = state[3]
+    self._hash2discount = state[4]
+    self._hash2idx = state[5]
+    self._transition_matrix = self.restore_transition_tensor(state[6])
+    self._idx2hash = state[7]
+    print(f'[GSM] Restored transition tensor {self._transition_matrix.shape}')
     print(f'[GSM] Took {time.time() - t0}s to restore from checkpoint.')
+
+  def restore_transition_tensor(self, transition_matrix):
+    k = self._tensor_increments
+    self._n_actions = ((len(self._hash2idx) // k) + 1) * k
+    self._n_states = self._n_actions
+    transition_tensor = np.zeros(
+      (self._n_states, self._n_actions), dtype=np.float32)
+    n_real_nodes = len(transition_matrix)
+    transition_tensor[:n_real_nodes, :n_real_nodes] = transition_matrix
+    return transition_tensor
