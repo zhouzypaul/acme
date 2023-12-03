463caac8b6bb485052824d4a04246ebf744f13db
Akhil Bagaria <abagaria@hmc.edu>
2023-12-02 19:27:19 -0500

    Added learning curve plotting from cfn branch.

---
+++ examples/baselines/rl_discrete/plotting/comparison_plotting.py
@@ -0,0 +1,208 @@
+import os
+import re
+import glob
+import numpy as np
+import matplotlib.pyplot as plt
+from collections import defaultdict
+from comparison_plotting_utils import *
+from plotting_utils import get_summary_data, extract_exploration_amounts, load_count_dict, get_true_vs_approx
+
+
+def get_config(acme_id, group_key):
+    try:
+        return re.search(f".*?([+-]+{group_key}|{group_key}_[^_]*)_.*", acme_id).group(1)
+    except: # if its at the end of the id name
+        return re.search(f".*?([+-]+{group_key}|{group_key}_[^_]*)$", acme_id).group(1)
+
+def default_make_key(log_dir_name, group_keys):
+    keys = [get_config(log_dir_name, group_key) for group_key in group_keys]
+    key = "_".join(keys)
+    return key
+
+
+def extract_log_dirs(id_to_csv, group_keys=("rewardscale",), xkey='actor_steps', ykey='episode_return'):
+
+    # Map config to a list of curves
+    log_dir_map = defaultdict(list)
+
+    for acme_id, csv_path in id_to_csv.items():
+        try:
+            keys = [get_config(acme_id, group_key) for group_key in group_keys]
+            key = "_".join(keys)
+            key = default_make_key(acme_id, group_keys)
+            # key = get_config(log_dir, group_key)
+            frames, returns = get_summary_data(csv_path, xkey=xkey, ykey=ykey)
+            log_dir_map[key].append((frames, returns))
+        except Exception as e:
+            print(f"Could not extract {acme_id}")
+            print(e)
+
+
+            # import ipdb; ipdb.set_trace()
+            # print('why')
+
+    return log_dir_map
+
+def extract_log_dirs_group_func(id_to_csv, group_func=lambda x: x, xkey='actor_steps', ykey='episode_return'):
+
+    # Map config to a list of curves
+    log_dir_map = defaultdict(list)
+
+    for acme_id, csv_path in id_to_csv.items():
+        try:
+            key = group_func(acme_id)
+            if key is None:
+                continue
+            # key = get_config(log_dir, group_key)
+            # frames, returns = get_summary_data(csv_path, xkey='actor_steps', ykey='episode_return')
+            frames, returns = get_summary_data(csv_path, xkey=xkey, ykey=ykey)
+            log_dir_map[key].append((frames, returns))
+        except:
+            print(f"Could not extract from {acme_id}")
+
+    return log_dir_map
+
+
+def plot_comparison_learning_curves(
+    # id_to_csv, # dict
+    base_dir, #str
+    selected_acme_ids=None,
+    # experiment_name=None,
+    # stat='eval_episode_lengths',
+    group_keys=("rewardscale",),
+    group_func=None,
+    filter_func=None, # Only include things that are "true" in filter
+    save_path=None,
+    show=True,
+    smoothen=10,
+    log_dir_path_map=None,
+    uniform_truncate=False,
+    truncate_max_frames=-1,
+    truncate_min_frames=-1,
+    ylabel=False,
+    legend_loc=None,
+    linewidth=2,
+    min_seeds=1,
+    all_seeds=False,
+    title=None,
+    min_final_val=None,
+    log_file_type='evaluator',
+    xkey='actor_steps',
+    ykey='episode_return'):
+
+    # import seaborn as sns
+    # NUM_COLORS=100
+    # clrs = sns.color_palette('husl', n_colors=NUM_COLORS)
+    # sns.set_palette(clrs)
+    id_to_csv = gather_csv_files_from_base_dir(base_dir=base_dir, selected_acme_ids=selected_acme_ids, log_type=log_file_type)
+
+    assert isinstance(group_keys, (tuple, list)), f"{type(group_keys)} should be tuple or list"
+    if save_path is not None:
+        plt.figure(figsize=(24,12))
+
+
+    ylabel = ylabel or "Average Return"
+
+    if log_dir_path_map is None:
+        if group_func is not None:
+            log_dir_path_map = extract_log_dirs_group_func(id_to_csv=id_to_csv, group_func=group_func, xkey=xkey, ykey=ykey)
+        else:
+            log_dir_path_map = extract_log_dirs(id_to_csv=id_to_csv, group_keys=group_keys, xkey=xkey, ykey=ykey)
+
+    for config in log_dir_path_map:
+        if config is None:
+            continue
+        if filter_func and not filter_func(config):
+            continue
+        curves = log_dir_path_map[config]
+        print(config)
+        for curve in curves:
+            print(f"\t{len(curve[0])}")
+        truncated_xs, truncated_all_ys = truncate_and_interpolate(curves, max_frames=truncate_max_frames, min_frames=truncate_min_frames)
+        print(truncated_xs.shape)
+        # import ipdb; ipdb.set_trace()
+        if len(truncated_all_ys) < min_seeds:
+            continue
+        if smoothen and smoothen > 0 and len(truncated_xs) < smoothen:
+            continue
+
+        if min_final_val is not None:
+            if np.array(truncated_all_ys)[:, -1].mean() <= min_final_val:
+                continue
+
+        # score_array = np.array(truncated_all_ys)
+        print(np.max(truncated_all_ys))
+        generate_plot(
+            # score_array,
+            truncated_xs,
+            truncated_all_ys,
+            label=config,
+            smoothen=smoothen,
+            linewidth=linewidth,
+            all_seeds=all_seeds)
+    
+    # plt.grid()
+    plt.xlabel("Frames")
+    plt.ylabel(ylabel)
+    if title:
+        plt.title(title)
+
+    if show:
+        if legend_loc:
+            plt.legend(loc=legend_loc)
+        else:
+            plt.legend()
+        plt.show()
+    
+    if save_path is not None:
+        plt.legend()
+        plt.savefig(save_path)
+        plt.close()
+
+
+def get_rmse_for_each_iteration(count_dict):
+    exact, approx = get_true_vs_approx(count_dict, "bonus")
+    assert len(exact) == len(approx)
+    exact = np.asarray(exact)
+    approx = np.asarray(approx)
+    sq_errors = (exact-approx) ** 2
+    root_mean_sq_errors = np.mean(sq_errors) ** 0.5
+    return root_mean_sq_errors
+
+
+
+if __name__ == "__main__":
+    domain = 'fourrrooms'
+    base_dir = f"local_testing/sweeps/{domain}"
+    save_path = f"local_testing/sweeps/{domain}/dsg_n_sigmas_sweep_fourrooms.png"
+    
+    def lr_group_func(acme_id):
+        if "spi_3" not in acme_id:
+            return None
+        return get_config(acme_id, "learningrate")
+    def rc_group_func(acme_id):
+        if "spi_3" not in acme_id:
+            return None
+        return get_config(acme_id, "rewardcoefficient")
+    def new_group_func(acme_id):
+        # if "size3" not in acme_id or "size3_5" in acme_id:
+        #     return None
+        return default_make_key(acme_id, ("nsigmasthresholdforgoalcreation",))
+
+    plot_comparison_learning_curves(
+        base_dir=base_dir,
+        # save_path=None,
+        # show=True,
+        save_path=save_path,
+        show=False,
+        # group_keys=("cfnmaxreplaysize", "cfn_use_forgetting"),
+        # group_keys=("learningrate", ),
+        group_func=new_group_func,
+        # filter_func=lambda acme_id: "size3" in acme_id and "size3_5" not in acme_id,
+        smoothen=10,
+        # smoothen=False,
+        # truncate_min_frames=50_000_000,
+        # min_seeds=5,
+        all_seeds=False,
+        title=f"DSG FourRooms (n_sigmas) {domain}",
+        )
---
+++ examples/baselines/rl_discrete/plotting/comparison_plotting_utils.py
@@ -0,0 +1,125 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy
+import os
+
+# def truncate(scores, max_length=-1, min_length=-1):
+#     filtered_scores = [score_list for score_list in scores if len(score_list) > min_length]
+#     if not filtered_scores:
+#         return filtered_scores
+#     min_length = min([len(x) for x in filtered_scores])
+#     if max_length > 0:
+#         min_length = min(min_length, max_length)
+#     truncated_scores = [score[:min_length] for score in filtered_scores]
+    
+#     return truncated_scores
+
+# def gather_evaluator_csv_files_from_base_dir(base_dir=None, selected_acme_ids=None):
+def gather_csv_files_from_base_dir(base_dir=None, selected_acme_ids=None, log_type='evaluator'):
+    """
+    Here the base_dir is assumed to be ~/acme, and under the base_dir are the dirs
+    named with `acme_id`, under which has `logs/evaluator/logs.csv`.
+
+    Args:
+        base_dir (_type_): acme results dir, if None use the default acme setting
+        selected_acme_ids (_type_): if None, use all acme_ids, otherwise use the selected ones
+        
+    Returns:
+        a dict of {acme_id: csv_path}
+    """
+    if base_dir is None:
+        base_dir = os.path.expanduser('~/acme')
+
+    # find all subdir and get the acme_id of all
+    id_to_csv = {}
+    for acme_id in os.listdir(base_dir):
+        if selected_acme_ids is not None and acme_id not in selected_acme_ids:
+            continue
+        exp_dir = os.path.join(base_dir, acme_id)
+        if not os.path.isdir(exp_dir):
+            continue
+        # csv_path = os.path.join(exp_dir, 'logs', 'evaluator', 'logs.csv')
+        csv_path = os.path.join(exp_dir, 'logs', log_type, 'logs.csv')
+        id_to_csv[acme_id] = csv_path
+
+    return id_to_csv
+
+def interpolate_xys(scores):
+    all_xs = [frames for (frames, returns) in scores]
+    flattened_unique = np.sort(np.unique(np.concatenate(all_xs)))
+    all_returns = []
+    for frames, returns in scores:
+        f = scipy.interpolate.interp1d(frames, returns, kind='nearest', fill_value='extrapolate')
+        interpolated_returns = f(flattened_unique)
+        all_returns.append(interpolated_returns)
+
+    return np.array(flattened_unique), np.array(all_returns)
+
+def truncate_and_interpolate(scores, max_frames=-1, min_frames=-1):
+    filtered_scores = [(frames, returns) for (frames, returns) in scores if max(frames) > min_frames]
+    if not filtered_scores:
+        return filtered_scores
+    assert all(all(frames[i] <= frames[i+1] for i in range(len(frames) - 1)) for (frames, returns) in filtered_scores), "needs to be sorted for what follows"
+    min_max_frames = min([max(frames) for (frames, returns) in filtered_scores])
+    min_max_frames = min(min_max_frames, max_frames)
+    shortened_scores = []
+    for frames, returns in filtered_scores:
+        if min_max_frames > 0:
+            frames = frames[frames <= max_frames]
+            returns = returns[:len(frames)]
+        shortened_scores.append((frames, returns))
+    
+    xs, all_ys = interpolate_xys(shortened_scores)
+    return xs, all_ys
+
+
+
+
+def get_plot_params(array):
+    median = np.median(array, axis=0)
+    means = np.mean(array, axis=0)
+    std = np.std(array, axis=0)
+    N = array.shape[0]
+    top = means + (std / np.sqrt(N))
+    bot = means - (std / np.sqrt(N))
+    return median, means, top, bot
+
+
+def moving_average(a, n=25):
+    ret = np.cumsum(a, dtype=float)
+    ret[n:] = ret[n:] - ret[:-n]
+    return ret[n-1:] / n
+
+
+def smoothen_data(scores, n=10):
+    smoothened_cols = scores.shape[1] - n + 1
+    smoothened_data = np.zeros((scores.shape[0], smoothened_cols))
+    for i in range(scores.shape[0]):
+        smoothened_data[i, :] = moving_average(scores[i, :], n=n)
+    return smoothened_data
+
+# def generate_plot(score_array, label, smoothen=0, linewidth=2, all_seeds=False):
+#     # smoothen is a number of iterations to average over
+#     if smoothen > 0:
+#         score_array = smoothen_data(score_array, n=smoothen)
+#     median, mean, top, bottom = get_plot_params(score_array)
+#     plt.plot(mean, linewidth=linewidth, label=label, alpha=0.9)
+#     plt.fill_between( range(len(top)), top, bottom, alpha=0.2 )
+#     if all_seeds:
+#         for i, score in enumerate(score_array):
+#             plt.plot(score, linewidth=linewidth, label=label+f"_{i+1}", alpha=0.6)
+
+def generate_plot(xs, all_ys, label, smoothen=0, linewidth=2, all_seeds=False):
+    # smoothen is a number of iterations to average over. Much less meaningful now that we have weird x axis.
+    if smoothen > 0:
+        xs = moving_average(xs, n=smoothen)
+        all_ys = smoothen_data(all_ys, n=smoothen)
+
+    median, mean, top, bottom = get_plot_params(all_ys)
+    plt.plot(xs, mean, linewidth=linewidth, label=label, alpha=0.9)
+    # plt.fill_between( range(len(top)), top, bottom, alpha=0.2 )
+    plt.fill_between( xs, top, bottom, alpha=0.2 )
+    if all_seeds:
+        for i, score in enumerate(all_ys):
+            plt.plot(xs, score, linewidth=linewidth, label=label+f"_{i+1}", alpha=0.6)
+            
